80f9e68f1a9925750fbaa6e272f1a00cff763429
nevaquit <36525147+nevaquit@users.noreply.github.com>
2026-05-24 10:49:02 +0200

    Create main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>
+      <h1>ADEVE Creations</h1>
+      <p>Website coming soon.</p>
+    </div>
+  </StrictMode>,
+)
